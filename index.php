301f73bea1eaa7cca6579218320060187fb16e7b
josec888 <61056856+josec888@users.noreply.github.com>
2020-02-28 16:05:40 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>Prueba de PHP</title>
+ </head>
+ <body>
+ <?php echo '<p>Hola Mundo</p>'; ?>
+Modificado el 25 de febrero
+<br/>Modificación para desplegar
+ </body>
+</html>
